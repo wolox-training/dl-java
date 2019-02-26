8d60d6b15f8916c362991b3c5d15e02caa7b0e70
Daniela Lotero Tapias <daniela.loterotapias@wolox.co>
2019-02-26 14:15:57 -0300

    Revert "[User Repo]Adding user repo and findByUsername"

--- src/main/java/wolox/training/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package wolox.training.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import wolox.training.models.User;
-
-public interface UserRepository  extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-}
